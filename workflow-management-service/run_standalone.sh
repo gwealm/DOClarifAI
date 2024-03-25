3e81e619c565e6397704986ddee11391b42b510b
Pedro Gomes <pedromgomes29@gmail.com>
2024-03-25 19:09:53 +0000

    chore: workflow management service set up

---
+++ workflow-management-service/run_standalone.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+ENV_FILE="env/standalone.env"
+
+run_compose_without_env() {
+    docker compose \
+        -f compose.base.yml \
+        -f compose.standalone.yml \
+        $@
+}
+
+run_compose() {
+    if [ -f "$ENV_FILE" ]; then
+        run_compose_without_env --env-file "$ENV_FILE" $@
+    else
+        run_compose_without_env $@
+    fi
+}
+
+run_compose up app -d --build --remove-orphans
+run_compose logs app -f 
