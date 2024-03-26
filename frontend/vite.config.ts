41dd0d953c5aec46e55d342e8dba9fc1460775ab
Guilherme Almeida <gui1612@sapo.pt>
2024-03-26 19:20:40 +0000

    chore: sets up dockerized frontend
    sets up react, vite and vitest

---
+++ frontend/vite.config.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  test: {
+    // Add any custom rules if necessary
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  }
+});
