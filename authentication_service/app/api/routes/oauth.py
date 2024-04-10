24dd7b7f84643418a49914c6a07551bb09afc590
Pedro Gomes <pedromgomes29@gmail.com>
2024-04-10 22:39:02 +0100

    feat: authentication service initial setup (missing connection to DB)

---
+++ authentication_service/app/api/routes/oauth.py
@@ -0,0 +1,38 @@
+from typing import Any,Annotated
+from fastapi import APIRouter, HTTPException,Depends
+from datetime import timedelta
+from app.crud import users as crud_users
+from app.core import security
+from app.core.config import settings
+from fastapi.security import OAuth2PasswordRequestForm
+from app.api.deps import (
+    SessionDep,
+)
+
+from app.models.tokens import Token
+
+router = APIRouter()
+
+
+
+@router.post("/token")
+def login_access_token(
+    session: SessionDep, form_data: Annotated[OAuth2PasswordRequestForm, Depends()]
+) -> Token:
+    """
+    OAuth2 compatible token login, get an access token for future requests
+    """
+    user = crud_users.authenticate(
+        session=session, username=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return Token(
+        access_token=security.create_access_token(
+            user.id, expires_delta=access_token_expires
+        )
+    )
+
+
