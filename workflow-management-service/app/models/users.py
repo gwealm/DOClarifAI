d22f87d497aaa189179aa6677436722a64f18098
Pedro Gomes <pedromgomes29@gmail.com>
2024-04-17 16:54:39 +0100

    refactor: authentication service lint compliance

--- workflow-management-service/app/models/users.py
+++ workflow-management-service/app/models/users.py
@@ -1,19 +1,23 @@
+"""
+  This file contains the Pydantic models for the User entity.
+"""
 from sqlmodel import Field, SQLModel
 
 
+
+
 class UserBase(SQLModel):
+  """
+  The basic information of a user.
+  """
   username: str = Field(unique=True, index=True)
 
 
-class UserCreate(UserBase):
-  password: str
-
-
 class User(UserBase, table=True):
+  """
+    The ORM model for the User entity.
+  """
   id: int | None = Field(default=None, primary_key=True)
   hashed_password: str
-  #TODO: something like workflows: list["Workflow"] = Relationship(back_populates="owner")
-
-
-class UserPublic(UserBase):
-  id: int
+  #TODO: something like workflows:
+  #   list["Workflow"] = Relationship(back_populates="owner")
