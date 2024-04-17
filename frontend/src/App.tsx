9194a200785fea7465e2f354ffb70cc6d30390ec
Guilherme Almeida <gui1612@sapo.pt>
2024-04-17 15:54:45 +0100

    feat: adds support for changing themes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import {BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Workflows from './pages/Workflows';
-import Workflow from './pages/Workflow';
-import Header from './components/Header';
-import ProcessedFiles from './pages/ProcessedFiles';
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Workflows from "./pages/Workflows";
+import { ThemeProvider } from "./components/ThemeProvider"
+import Workflow from "./pages/Workflow";
+import Header from "./components/Header";
+import ProcessedFiles from "./pages/ProcessedFiles";
 
 function App() {
-  //const location = useLocation();
-  return (
-    <>
-      <Header />
-      <BrowserRouter>
-        <Routes location = {location} key = {location.pathname}>
-          <Route path="/workflows" element={<Workflows/>}/>
-          <Route path="/" element={<Workflows/>}/>
-          <Route path="/workflow/:id" element={<Workflow/>}/>
-          <Route path="/processedfiles" element={<ProcessedFiles/>}/>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
+    //const location = useLocation();
+    return (
+        <>
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                <Header />
+                <BrowserRouter>
+                    <Routes location={location} key={location.pathname}>
+                        <Route path="/workflows" element={<Workflows />} />
+                        <Route path="/" element={<Workflows />} />
+                        <Route path="/workflow/:id" element={<Workflow />} />
+                        <Route
+                            path="/processedfiles"
+                            element={<ProcessedFiles />}
+                        />
+                    </Routes>
+                </BrowserRouter>
+            </ThemeProvider>
+        </>
+    );
 }
 
 export default App;
