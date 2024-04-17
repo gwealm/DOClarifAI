57a2ad4a23c186b0755b971c019d71bbcba8c3ef
Martim Videira <mitram790@gmail.com>
2024-04-17 19:54:56 +0100

    fix: postgres settings import path

--- common/src/common/postgres/engine.py
+++ common/src/common/postgres/engine.py
@@ -2,7 +2,7 @@
 """
   This file is used to initialize the database connection.
 """
-from common.src.common.config import settings
+from common.config import settings
 from sqlmodel import create_engine
 
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
