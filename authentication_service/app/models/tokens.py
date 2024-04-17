64d0264a5feb8be0b2cbbfb863d46fdcbd37fd3d
Pedro Gomes <pedromgomes29@gmail.com>
2024-04-17 14:58:34 +0100

    refactor: authentication service models  lint compliance

--- authentication_service/app/models/tokens.py
+++ authentication_service/app/models/tokens.py
@@ -1,10 +1,19 @@
+"""
+  This module contains the pydantic models for the JWT tokens.
+"""
+
 from pydantic import BaseModel
 
-
+"""
+  The schema for the JWT token returned by the authentication endpoint.
+"""
 class Token(BaseModel):
   access_token: str
   token_type: str = "bearer"
 
 
+"""
+  The schema for the JWT token payload.
+"""
 class TokenPayload(BaseModel):
   sub: int | None = None
--- authentication_service/app/models/users.py
+++ authentication_service/app/models/users.py
@@ -1,19 +1,35 @@
+"""
+  This file contains the Pydantic models for the User entity.
+"""
 from sqlmodel import Field, SQLModel
 
 
+
+"""
+  The basic information of a user.
+"""
 class UserBase(SQLModel):
   username: str = Field(unique=True, index=True)
 
 
+
+"""
+  The information needed to create a new user.
+"""
 class UserCreate(UserBase):
   password: str
 
-
+"""
+  The ORM model for the User entity.
+"""
 class User(UserBase, table=True):
   id: int | None = Field(default=None, primary_key=True)
   hashed_password: str
   #TODO: something like workflows: list["Workflow"] = Relationship(back_populates="owner")
 
 
+"""
+  The public information of a user.
+"""
 class UserPublic(UserBase):
   id: int
