82d2197cf629c053ec0109cc9f0c9e38a4ec2ca5
Martim Videira <mitram790@gmail.com>
2024-04-29 14:12:35 +0100

    chore: refactor to follow linter rules

--- workflow-management-service/app/deps.py
+++ workflow-management-service/app/deps.py
@@ -5,13 +5,12 @@ from typing import Annotated
 
 from fastapi import Depends, HTTPException, status
 
-
 from common.models.workflows import Workflow
 from common.deps import CurrentUser
 
 
 def get_path_workflow(workflow_id: int, current_user: CurrentUser) -> Workflow:
-    pass
+  pass
 
 
-PathWorkflow =  Annotated[Workflow,Depends(get_path_workflow)]
+PathWorkflow = Annotated[Workflow, Depends(get_path_workflow)]
