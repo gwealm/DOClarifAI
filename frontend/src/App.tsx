6a60c8b52e7f467b84933a6706c0933e7a109f44
Guilherme Almeida <gui1612@sapo.pt>
2024-05-22 18:38:00 +0100

    feat: removes dark theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import "./App.css";
-import { ThemeProvider } from "./components/ThemeProvider"
 import Router from "./Router";
 
 function App() {
     return (
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-            <Router />
-        </ThemeProvider>
+        <Router />
     );
 }
 
